9040185643209cd4adc786895cc1b3c8d8bb478e
Phạm Minh Trọng <trongpro0307@gmail.com>
2024-03-09 17:58:49 +0700

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-        // Test MT202 to pacs.009 conversion
+//        testMT202ToPACS009Conversion();
+//        testMT900ToCAMT054Conversion();
+//        testMT910ToCAMT054Conversion();
+        testMT515ToMXConversion();
+    }
+
+    public static void testMT202ToPACS009Conversion() {
         MT202ToPACS009Converter mt202ToPacs009Converter = new MT202ToPACS009Converter();
         String mt202Message =
-                ":20:REFERENCE\n" +
-                        ":52A:SENDERBANK\n" +
-                        ":57A:RECEIVERBANK\n" +
-                        ":32B:CURRENCYAMOUNT\n" +
-                        ":30:010101\n" +
-                        ":20:REFERENCE";
+                "{1:F01BANKUS33AXXX0000000000}{2:I202BANKNGBXXXXN}{4:\n" +
+                        ":20:1234567890n" +
+                        ":21:REF123456789\n" +
+                        ":32A:220106USD123456,78\n" +
+                        ":52A:/1234567890\n" +
+                        ":54A:/9876543210\n" +
+                        ":72:/TIEN CHUYEN KHOAN/\n" +
+                        "-}";
 
         String pacs009Message = mt202ToPacs009Converter.mt202ToPacs009(mt202Message);
         System.out.println("pacs.009 Message from MT202:");
         System.out.println(pacs009Message);
-//        // Test MT900 to CAMT.054 conversion
-//        MT900ToCAMT054Converter mt900ToCAMT054Converter = new MT900ToCAMT054Converter();
-//        String mt900Message =
-//                ":20:REFERENCE\n" +
-//                        ":25:ACCOUNT1234567890\n" +
-//                        ":28C:12345/1\n" +
-//                        ":60F:C240101USD12345,67\n" +
-//                        ":61:0101010101C12345,67NCHK1234567890123456\n" +
-//                        "DRAFT BANK\n" +
-//                        ":86:TRANSACTION DETAILS\n" +
-//                        "-";
-//
-//        String camt054Message = mt900ToCAMT054Converter.mt900ToCamt054(mt900Message);
-//        System.out.println("CAMT.054 Message from MT900:");
-//        System.out.println(camt054Message);
-//
-//        // Test MT910 to CAMT.054 conversion
-//        MT910ToCAMT054Converter mt910ToCAMT054Converter = new MT910ToCAMT054Converter();
-//        String mt910Message =
-//                ":20:REFERENCE\n" +
-//                        ":25:ACCOUNT1234567890\n" +
-//                        ":28C:12345/1\n" +
-//                        ":60F:C240101USD12345,67\n" +
-//                        ":61:0101010101C12345,67NCHK1234567890123456\n" +
-//                        "DRAFT BANK\n" +
-//                        ":86:TRANSACTION DETAILS\n" +
-//                        "-";
-//
-//        String camt054MessageFromMT910 = mt910ToCAMT054Converter.mt910ToCamt054(mt910Message);
-//        System.out.println("CAMT.054 Message from MT910:");
-//        System.out.println(camt054MessageFromMT910);
+    }
+
+    public static void testMT900ToCAMT054Conversion() {
+        MT900ToCAMT054Converter mt900ToCAMT054Converter = new MT900ToCAMT054Converter();
+        String mt900Message =
+                ":20:REFERENCE\n" +
+                        ":25:ACCOUNT1234567890\n" +
+                        ":28C:12345/1\n" +
+                        ":60F:C240101USD12345,67\n" +
+                        ":61:0101010101C12345,67NCHK1234567890123456\n" +
+                        "DRAFT BANK\n" +
+                        ":86:TRANSACTION DETAILS\n" +
+                        "-";
+
+        String camt054Message = mt900ToCAMT054Converter.mt900ToCamt054(mt900Message);
+        System.out.println("CAMT.054 Message from MT900:");
+        System.out.println(camt054Message);
+    }
+
+    public static void testMT910ToCAMT054Conversion() {
+        MT910ToCAMT054Converter mt910ToCAMT054Converter = new MT910ToCAMT054Converter();
+        String mt910Message =
+                ":20:REFERENCE\n" +
+                        ":25:ACCOUNT1234567890\n" +
+                        ":28C:12345/1\n" +
+                        ":60F:C240101USD12345,67\n" +
+                        ":61:0101010101C12345,67NCHK1234567890123456\n" +
+                        "DRAFT BANK\n" +
+                        ":86:TRANSACTION DETAILS\n" +
+                        "-";
+
+        String camt054MessageFromMT910 = mt910ToCAMT054Converter.mt910ToCamt054(mt910Message);
+        System.out.println("CAMT.054 Message from MT910:");
+        System.out.println(camt054MessageFromMT910);
+    }
+
+    public static void testMT515ToMXConversion() {
+        MT515ToMXConverter mt515ToMXConverter = new MT515ToMXConverter();
+        String mt515Message =
+                ":16R:CONFPRTY\n" +
+                        ":95P::INVE//JANIGB22\n" +
+                        ":97A::SAFE//11111111\n" +
+                        ":16R:CONFPRTY\n" +
+                        ":16R:CONFPRTY\n" +
+                        ":95P::SELL//PEFIGB22\n" +
+                        ":16R:CONFPRTY";
+
+        String mxMessage = mt515ToMXConverter.mt515ToMx(mt515Message);
+        System.out.println("MX Message from MT515:");
+        System.out.println(mxMessage);
     }
 }
